43acc137e3f5c002f3491a39d75be19b2e8950d6
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-13 00:07:00 -0700

    created new module input.js

---
+++ input.js
@@ -0,0 +1,21 @@
+const handleUserInput = function (data) {
+  console.log(data);
+
+  // your code here
+  if (data === '\u0003') process.exit();
+};
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const connect = require('./client');
+const setupInput = require('./input');
 
 console.log('Connecting ...');
 connect();
-
-const handleUserInput = function (data) {
-  console.log(data);
-
-  // your code here
-  if (data === '\u0003') process.exit();
-};
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput);
-
-  return stdin;
-};
 
 setupInput();
