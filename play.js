19494a97091127abfb599033a8ca516660b83054
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-12 23:34:07 -0700

    tidied code

--- play.js
+++ play.js
@@ -10,14 +10,9 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
-  conn.on('connect', () => {
-    // code that does something when the connection is first established
-    console.log('connected!');
-  });
+  conn.on('connect', () => console.log(`Connected to server!`));
 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
+  conn.on('data', (data) => console.log('Server says: ', data));
 
   return conn;
 };
